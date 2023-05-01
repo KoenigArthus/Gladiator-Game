213e12c8b8c8a39ac8f481d5ff8873240daa148b
KoenigArthus <jonathan.buss.aka.jo@gmail.com>
2023-05-01 14:14:22 +0200

    st

---
+++ GG/Assets/Scripts/Dialogue/LineViewCustom.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class LineViewCustom : LineView
+{
+    public override void UserRequestedViewAdvancement()
+    {
+        base.UserRequestedViewAdvancement();
+    }
+}
