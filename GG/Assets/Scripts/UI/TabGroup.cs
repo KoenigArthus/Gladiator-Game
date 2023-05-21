5f5e31ad36ad29f54284b824e0f29b790ad2f7de
KoenigArthus <jonathan.buss.aka.jo@gmail.com>
2023-05-21 22:26:12 +0200

    Inventory Dummy 50%

---
+++ GG/Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,104 @@
+using JSAM;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButtonG> tabButtons;
+    public TabButtonG selectedTab;
+    public List<GameObject> objectsToSwap;
+    [Tooltip("This prevents you from closing the Tab by clicking on the Button you used to open again")]
+    public bool thisGroupsTabsStayOpen;
+    [Tooltip("Will load the selected Tab onEnable if there is one")]
+    public bool LoadTabOnEnable;
+
+    public Sprite tabIdle;
+    public Sprite tabHover;
+    public Sprite tabSelected;
+
+
+
+    private void OnEnable()
+    {
+        if (LoadTabOnEnable)
+        {
+            if (selectedTab != null)
+            {
+                OnTabSelected(selectedTab);
+            }
+            else
+            {
+                Debug.Log("You are trying to Load a Tab OnEnable but selectedTab is null");
+            }
+        }
+    }
+
+    public void Subscribe(TabButtonG button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<TabButtonG>();
+        }
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButtonG button)
+    {
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.background.sprite = tabHover;
+        }
+    }
+
+    public void OnTabExit(TabButtonG button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButtonG button)
+    {
+        if ((selectedTab == button) && !thisGroupsTabsStayOpen)
+        {
+            selectedTab = null;
+            objectsToSwap[button.transform.GetSiblingIndex()].SetActive(false);
+            selectedTab.Select();
+        }
+        else
+        {
+            if(selectedTab != null)
+                selectedTab.Deselect();
+
+            selectedTab = button;
+            selectedTab.Select();
+            ResetTabs();
+            button.background.sprite = tabSelected;
+            int index = button.transform.GetSiblingIndex();
+            for (int i = 0; i < objectsToSwap.Count; i++)
+            {
+                if (i == index)
+                {
+                    objectsToSwap[i].SetActive(true);
+                }
+                else
+                {
+                    objectsToSwap[i].SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach (TabButtonG button in tabButtons)
+        {
+            if (selectedTab != null && button == selectedTab)
+                continue;
+
+            button.background.sprite = tabIdle;
+        }
+    }
+}
+
