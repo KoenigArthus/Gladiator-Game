afa80a50fdc2bbc3e34e08974c6d4ef7ba473a65
KoenigArthus <jonathan.buss.aka.jo@gmail.com>
2023-05-24 00:45:00 +0200

    aftermerge

--- GG/Assets/Scripts/Manager/LevelLoader.cs
+++ GG/Assets/Scripts/Manager/LevelLoader.cs
@@ -20,7 +20,7 @@ public class LevelLoader : MonoBehaviour
         }
         else if (i != this)
         {
-            Destroy(gameObject);
+            DestroyImmediate(gameObject);
         }
     }
 
