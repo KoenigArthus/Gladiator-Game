8a6fd8ba97483ecfddaa6af3df54014c288c4445
KoenigArthus <jonathan.buss.aka.jo@gmail.com>
2023-06-18 23:18:40 +0200

    property drawer basics

--- GG/Assets/Editor/TimeBoolTableDrawer.cs
+++ GG/Assets/Editor/TimeBoolTableDrawer.cs
@@ -4,41 +4,59 @@ using UnityEngine;
 [CustomPropertyDrawer(typeof(TimeBoolTable))]
 public class TimeBoolTableDrawer : PropertyDrawer
 {
-    public override void OnGUI(Rect a_Rect, SerializedProperty a_Property, GUIContent a_Label)
+    private string[] weekdays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    private const int NumColumns = 5;
+    private const int NumRows = 7;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        //Shorthand to make writing easier
-        float lh = EditorGUIUtility.singleLineHeight;
+        // Shorthand to make writing easier
+        float lineHeight = EditorGUIUtility.singleLineHeight;
 
-        //Draw our label in the full rect (will self centre) but leave room at the end for the bools
-        Rect labelRect = new Rect(a_Rect.x, a_Rect.y, a_Rect.width - lh * 6f, a_Rect.height);
-        EditorGUI.LabelField(labelRect, a_Label);
+        // Calculate the width of each bool cell, accounting for spacing
+        float cellWidth = (position.width - lineHeight * NumColumns) / NumColumns;
 
-        //This is where the bools will start on the x-axis
-        float boolStartX = a_Rect.x + labelRect.width;
-        //Initialise the bool rect to the top-left corner (1 line wide and tall)
-        Rect boolRect = new Rect(boolStartX, a_Rect.y, lh, lh);
-        //We want the bools to go left -> right then top -> bottom, so start with the y-axis as the outside loop
-        for (int y = 0; y < 7; y++)
+        // Calculate the initial position for the bool cells
+        Rect cellRect = new Rect(position.x, position.y, cellWidth, lineHeight);
+
+        // Disable the default spacing between PropertyFields
+        int originalIndentLevel = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        // Loop through rows and columns to draw the bool table
+        for (int row = 0; row < NumRows; row++)
         {
-            //Re-initialise the x-axis with every new row
-            boolRect.x = boolStartX;
-            for (int x = 0; x < 5; x++)
+            // Draw the weekday label for the current row
+            Rect rowLabelRect = new Rect(position.x, cellRect.y, 100f, lineHeight);
+            EditorGUI.LabelField(rowLabelRect, weekdays[row]);
+
+            // Move to the position of the first cell in the row
+            cellRect.x = position.x + 100f;
+
+            for (int column = 0; column < NumColumns; column++)
             {
-                //Find the current index (starting at 1)
-                int currentBool = x + y * 5 + 1;
-                //Draw the bool as normal (without a label)
-                EditorGUI.PropertyField(boolRect, a_Property.FindPropertyRelative("time" + currentBool), GUIContent.none);
-                //Move to the next bool in the row
-                boolRect.x += boolRect.height;
+                // Calculate the index of the current bool (starting at 1)
+                int currentIndex = column + row * NumColumns + 1;
+
+                // Draw the bool as normal (without a label) with zero spacing
+                EditorGUI.PropertyField(cellRect, property.FindPropertyRelative("time" + currentIndex), GUIContent.none);
+
+                // Move to the next cell in the row
+                cellRect.x += cellRect.width;
             }
-            //Move to the next row of bools
-            boolRect.y += boolRect.width;
+
+            // Move to the next row of cells
+            cellRect.x = position.x;
+            cellRect.y += lineHeight;
         }
+
+        // Restore the original indent level
+        EditorGUI.indentLevel = originalIndentLevel;
     }
 
-    //We need to override the property height 
-    public override float GetPropertyHeight(SerializedProperty a_Property, GUIContent a_Label)
+    // We need to override the property height - let's make it 7 lines (a regular property will be 1 line high)
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        return EditorGUIUtility.singleLineHeight * 7f;
+        return EditorGUIUtility.singleLineHeight * NumRows;
     }
 }
